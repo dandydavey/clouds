a78a620f7fb8e0bbdd26d8f3c14677d788b85f40
David Ye <dave.z.ye@gmail.com>
2024-09-24 15:53:31 -0700

    Add effect and click layers

--- src/components/CloudSvg.tsx
+++ src/components/CloudSvg.tsx
@@ -1,18 +1,18 @@
 import React from "react";
 
 interface CloudSvgProps {
-  clicked: boolean;
-  onClick: () => void;
+  clickedIndices: boolean[];
+  setClickedIndices: React.Dispatch<React.SetStateAction<boolean[]>>;
   paths: string[];
 }
 
-export default function CloudSvg({ clicked, onClick, paths }: CloudSvgProps) {
+export function EffectAreas({ clickedIndices, paths }: CloudSvgProps) {
   return (
     <>
       {paths.map((path, index) => (
         <svg
           key={index}
-          className={`absolute ${clicked ? "clicked" : ""}`}
+          className={`absolute ${clickedIndices[index] ? "clicked" : ""}`}
           style={{ top: 0, left: 0, width: "100%", height: "100%" }}
           viewBox="0 0 2560 1423"
         >
@@ -30,12 +30,52 @@ export default function CloudSvg({ clicked, onClick, paths }: CloudSvgProps) {
           <rect
             width="100%"
             height="100%"
-            fill="transparent"
-            onClick={onClick}
+            // fill="red"
+            opacity={0}
             clipPath={`url(#clip-path-${index})`}
           />
         </svg>
       ))}
     </>
+  );
+}
+
+export default function ClickAreas({
+  setClickedIndices,
+  paths,
+}: CloudSvgProps) {
+  const handleClick = (index: number) => {
+    console.log("clicked ", index);
+    setClickedIndices((prev) => {
+      const newClickedPaths = [...prev];
+      newClickedPaths[index] = !newClickedPaths[index];
+      return newClickedPaths;
+    });
+  };
+
+  return (
+    <svg
+      className={`absolute`}
+      style={{ top: 0, left: 0, width: "100%", height: "100%" }}
+      viewBox="0 0 2560 1423"
+    >
+      <defs>
+        {paths.map((path, index) => (
+          <clipPath key={`clip-${index}`} id={`clip-path-${index}`}>
+            <path d={path} />
+          </clipPath>
+        ))}
+      </defs>
+      {paths.map((_, index) => (
+        <rect
+          key={`rect-${index}`}
+          width="100%"
+          height="100%"
+          opacity={0}
+          onClick={() => handleClick(index)}
+          clipPath={`url(#clip-path-${index})`}
+        />
+      ))}
+    </svg>
   );
 }
